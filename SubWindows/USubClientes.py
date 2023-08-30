0e7362dc110904c91c21be0c694052d69a070539
Thiago-sys <77471758+Thiago-sys@users.noreply.github.com>
2023-08-30 00:14:27 -0300

    feat: Criando as novas classes das subWindows

---
+++ SubWindows/USubClientes.py
@@ -0,0 +1,116 @@
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QIcon
+
+
+class SubWindowClientes(QtWidgets.QWidget):
+    def __init__(self, db):
+        super().__init__()
+        self.db = db
+
+        self.subClientes = QtWidgets.QWidget()
+        self.subClientes.setObjectName("subClientes")
+        self.verticalLayout_7 = QtWidgets.QVBoxLayout(self.subClientes)
+        self.verticalLayout_7.setObjectName("verticalLayout_7")
+        self.gridClientes = QtWidgets.QTableWidget(self.subClientes)
+        self.gridClientes.setObjectName("gridClientes")
+        self.gridClientes.setColumnCount(6)
+        self.gridClientes.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridClientes.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridClientes.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridClientes.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridClientes.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridClientes.setHorizontalHeaderItem(4, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridClientes.setHorizontalHeaderItem(5, item)
+        self.gridClientes.horizontalHeader().setDefaultSectionSize(120)
+        self.gridClientes.horizontalHeader().setMinimumSectionSize(100)
+        self.verticalLayout_7.addWidget(self.gridClientes)
+        self.fraBotoesClientes = QtWidgets.QFrame(self.subClientes)
+        self.fraBotoesClientes.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.fraBotoesClientes.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.fraBotoesClientes.setObjectName("fraBotoesClientes")
+        self.horizontalLayout_7 = QtWidgets.QHBoxLayout(self.fraBotoesClientes)
+        self.horizontalLayout_7.setObjectName("horizontalLayout_7")
+        self.btnInserirClientes = QtWidgets.QPushButton(self.fraBotoesClientes)
+        self.btnInserirClientes.setMinimumSize(QtCore.QSize(48, 48))
+        self.btnInserirClientes.setMaximumSize(QtCore.QSize(48, 48))
+        self.btnInserirClientes.setStyleSheet("background-color: transparent; \n"
+                                              "border: none;")
+        self.btnInserirClientes.setText("")
+        self.btnInserirClientes.setIcon(QIcon("icons/2x/insert.png"))
+        self.btnInserirClientes.setIconSize(QtCore.QSize(48, 48))
+        self.btnInserirClientes.setObjectName("btnInserirClientes")
+        self.horizontalLayout_7.addWidget(self.btnInserirClientes)
+        self.btnEditarClientes = QtWidgets.QPushButton(self.fraBotoesClientes)
+        self.btnEditarClientes.setMinimumSize(QtCore.QSize(48, 48))
+        self.btnEditarClientes.setStyleSheet("background-color: transparent; \n"
+                                             "border: none;")
+        self.btnEditarClientes.setText("")
+        self.btnEditarClientes.setIcon(QIcon("icons/2x/edit.png"))
+        self.btnEditarClientes.setIconSize(QtCore.QSize(36, 36))
+        self.btnEditarClientes.setObjectName("btnEditarClientes")
+        self.horizontalLayout_7.addWidget(self.btnEditarClientes)
+        self.btnExcluirClientes = QtWidgets.QPushButton(self.fraBotoesClientes)
+        self.btnExcluirClientes.setMinimumSize(QtCore.QSize(48, 48))
+        self.btnExcluirClientes.setStyleSheet("background-color: transparent; \n"
+                                              "border: none;")
+        self.btnExcluirClientes.setText("")
+        self.btnExcluirClientes.setIcon(QIcon("icons/2x/delete.png"))
+        self.btnExcluirClientes.setIconSize(QtCore.QSize(36, 36))
+        self.btnExcluirClientes.setObjectName("btnExcluirClientes")
+        self.horizontalLayout_7.addWidget(self.btnExcluirClientes)
+        spacerItem5 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_7.addItem(spacerItem5)
+        self.btnFecharClientes = QtWidgets.QPushButton(self.fraBotoesClientes)
+        self.btnFecharClientes.setMinimumSize(QtCore.QSize(48, 48))
+        self.btnFecharClientes.setMaximumSize(QtCore.QSize(48, 48))
+        self.btnFecharClientes.setStyleSheet("background-color: transparent; \n"
+                                             "border: none;")
+        self.btnFecharClientes.setText("")
+        self.btnFecharClientes.setIcon(QIcon("icons/2x/close.png"))
+        self.btnFecharClientes.setIconSize(QtCore.QSize(40, 40))
+        self.btnFecharClientes.setObjectName("btnFecharClientes")
+        self.horizontalLayout_7.addWidget(self.btnFecharClientes)
+        self.verticalLayout_7.addWidget(self.fraBotoesClientes)
+
+        _translate = QtCore.QCoreApplication.translate
+        self.subClientes.setWindowTitle(_translate("MainWindow", "Subwindow"))
+        item = self.gridClientes.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "Código"))
+        item = self.gridClientes.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Descrição"))
+        item = self.gridClientes.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "Inscrição"))
+        item = self.gridClientes.horizontalHeaderItem(3)
+        item.setText(_translate("MainWindow", "Telefone"))
+        item = self.gridClientes.horizontalHeaderItem(4)
+        item.setText(_translate("MainWindow", "Email"))
+        item = self.gridClientes.horizontalHeaderItem(5)
+        item.setText(_translate("MainWindow", "Endereço"))
+
+        self.btnInserirClientes.clicked.connect(self.inserirClientes)
+        self.btnEditarClientes.clicked.connect(lambda event: self.editarClientes(self.gridClientes))
+        self.btnExcluirClientes.clicked.connect(lambda event: self.excluirClientes(self.gridClientes))
+
+        self.buscarClientes(self.gridClientes)
+        self.gridClientes.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+
+    def get(self):
+        return self.subClientes
+
+    def inserirClientes(self):
+        pass
+
+    def editarClientes(self, gridClientes):
+        pass
+
+    def excluirClientes(self, gridClientes):
+        pass
+
+    def buscarClientes(self, gridClientes):
+        pass
---
+++ SubWindows/USubFornecedores.py
@@ -0,0 +1,119 @@
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QIcon
+from UCadLanDialog import CadLanDialog
+from UCustomMessageBox import CustomMessageBox
+from datetime import datetime
+
+
+class SubWindowFornecedorescamentos(QtWidgets.QWidget):
+    def __init__(self, db):
+        super().__init__()
+        self.db = db
+
+        self.subFornecedores = QtWidgets.QWidget()
+        self.subFornecedores.setObjectName("subFornecedores")
+        self.verticalLayout_8 = QtWidgets.QVBoxLayout(self.subFornecedores)
+        self.verticalLayout_8.setObjectName("verticalLayout_8")
+        self.gridFornecedores = QtWidgets.QTableWidget(self.subFornecedores)
+        self.gridFornecedores.setObjectName("gridFornecedores")
+        self.gridFornecedores.setColumnCount(6)
+        self.gridFornecedores.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridFornecedores.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridFornecedores.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridFornecedores.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridFornecedores.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridFornecedores.setHorizontalHeaderItem(4, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.gridFornecedores.setHorizontalHeaderItem(5, item)
+        self.gridFornecedores.horizontalHeader().setDefaultSectionSize(120)
+        self.gridFornecedores.horizontalHeader().setMinimumSectionSize(100)
+        self.verticalLayout_8.addWidget(self.gridFornecedores)
+        self.fraBotoesFornecedores = QtWidgets.QFrame(self.subFornecedores)
+        self.fraBotoesFornecedores.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.fraBotoesFornecedores.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.fraBotoesFornecedores.setObjectName("fraBotoesFornecedores")
+        self.horizontalLayout_8 = QtWidgets.QHBoxLayout(self.fraBotoesFornecedores)
+        self.horizontalLayout_8.setObjectName("horizontalLayout_8")
+        self.btnInserirFornecedores = QtWidgets.QPushButton(self.fraBotoesFornecedores)
+        self.btnInserirFornecedores.setMinimumSize(QtCore.QSize(48, 48))
+        self.btnInserirFornecedores.setMaximumSize(QtCore.QSize(48, 48))
+        self.btnInserirFornecedores.setStyleSheet("background-color: transparent; \n"
+                                                  "border: none;")
+        self.btnInserirFornecedores.setText("")
+        self.btnInserirFornecedores.setIcon(QIcon("icons/2x/insert.png"))
+        self.btnInserirFornecedores.setIconSize(QtCore.QSize(48, 48))
+        self.btnInserirFornecedores.setObjectName("btnInserirFornecedores")
+        self.horizontalLayout_8.addWidget(self.btnInserirFornecedores)
+        self.btnEditarFornecedores = QtWidgets.QPushButton(self.fraBotoesFornecedores)
+        self.btnEditarFornecedores.setMinimumSize(QtCore.QSize(48, 48))
+        self.btnEditarFornecedores.setStyleSheet("background-color: transparent; \n"
+                                                 "border: none;")
+        self.btnEditarFornecedores.setText("")
+        self.btnEditarFornecedores.setIcon(QIcon("icons/2x/edit.png"))
+        self.btnEditarFornecedores.setIconSize(QtCore.QSize(36, 36))
+        self.btnEditarFornecedores.setObjectName("btnEditarFornecedores")
+        self.horizontalLayout_8.addWidget(self.btnEditarFornecedores)
+        self.btnExcluirFornecedores = QtWidgets.QPushButton(self.fraBotoesFornecedores)
+        self.btnExcluirFornecedores.setMinimumSize(QtCore.QSize(48, 48))
+        self.btnExcluirFornecedores.setStyleSheet("background-color: transparent; \n"
+                                                  "border: none;")
+        self.btnExcluirFornecedores.setText("")
+        self.btnExcluirFornecedores.setIcon(QIcon("icons/2x/delete.png"))
+        self.btnExcluirFornecedores.setIconSize(QtCore.QSize(36, 36))
+        self.btnExcluirFornecedores.setObjectName("btnExcluirFornecedores")
+        self.horizontalLayout_8.addWidget(self.btnExcluirFornecedores)
+        spacerItem6 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_8.addItem(spacerItem6)
+        self.btnFecharFornecedores = QtWidgets.QPushButton(self.fraBotoesFornecedores)
+        self.btnFecharFornecedores.setMinimumSize(QtCore.QSize(48, 48))
+        self.btnFecharFornecedores.setMaximumSize(QtCore.QSize(48, 48))
+        self.btnFecharFornecedores.setStyleSheet("background-color: transparent; \n"
+                                                 "border: none;")
+        self.btnFecharFornecedores.setText("")
+        self.btnFecharFornecedores.setIcon(QIcon("icons/2x/close.png"))
+        self.btnFecharFornecedores.setIconSize(QtCore.QSize(40, 40))
+        self.btnFecharFornecedores.setObjectName("btnFecharFornecedores")
+        self.horizontalLayout_8.addWidget(self.btnFecharFornecedores)
+        self.verticalLayout_8.addWidget(self.fraBotoesFornecedores)
+
+        _translate = QtCore.QCoreApplication.translate
+        self.subFornecedores.setWindowTitle(_translate("MainWindow", "Subwindow"))
+        item = self.gridFornecedores.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "Código"))
+        item = self.gridFornecedores.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Descrição"))
+        item = self.gridFornecedores.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "Inscrição"))
+        item = self.gridFornecedores.horizontalHeaderItem(3)
+        item.setText(_translate("MainWindow", "Telefone"))
+        item = self.gridFornecedores.horizontalHeaderItem(4)
+        item.setText(_translate("MainWindow", "Email"))
+        item = self.gridFornecedores.horizontalHeaderItem(5)
+        item.setText(_translate("MainWindow", "Endereço"))
+
+        self.btnInserirFornecedores.clicked.connect(self.inserirFornecedores)
+        self.btnEditarFornecedores.clicked.connect(lambda event: self.editarFornecedores(self.gridFornecedores))
+        self.btnExcluirFornecedores.clicked.connect(lambda event: self.excluirFornecedores(self.gridFornecedores))
+
+        self.buscarFornecedores(self.gridFornecedores)
+        self.gridFornecedores.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+
+    def get(self):
+        return self.subFornecedores
+
+    def inserirFornecedores(self):
+        pass
+
+    def editarFornecedores(self, gridFornecedores):
+        pass
+
+    def excluirFornecedores(self, gridFornecedores):
+        pass
+
+    def buscarFornecedorescamentos(self, gridFornecedores):
+        pass
