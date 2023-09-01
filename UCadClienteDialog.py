11c4e6e06d64ef6b18bb777a76a8dd7586034d3a
Thiago-sys <77471758+Thiago-sys@users.noreply.github.com>
2023-08-31 21:32:14 -0300

    feat: Finalizada a parte de cadastro de cliente e fornecedores

--- UCadClienteDialog.py
+++ UCadClienteDialog.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QDialog
 from Modelos.CadCliente import Ui_Dialog
 from UDataBaseManager import DatabaseManager
+from UCustomMessageBox import CustomMessageBox
+from PyQt5 import QtWidgets
 
 
 class CadClienteDialog(QDialog, Ui_Dialog):
@@ -11,3 +13,60 @@ class CadClienteDialog(QDialog, Ui_Dialog):
         self.db = db
         self.state = state
         self.codigo = codigo
+
+        self.btnGravar.clicked.connect(self.gravar)
+        self.btnCancelar.clicked.connect(self.cancelar)
+
+    def gravar(self):
+        try:
+            if self.db.connection.in_transaction:
+                self.db.connection.rollback()
+            self.db.connection.start_transaction()
+
+            nome = self.edtNome.text()
+            inscricao = self.edtCpf.text()
+            telefone = self.edtTelefone.text()
+            email = self.edtEmail.text()
+            endereco = self.edtEnd.text()
+
+            if self.state == "Insert":
+                result = CustomMessageBox("Confirmar Gravação",
+                                          "Deseja realmente salvar esse cliente?").confirmation.exec_()
+                if result == QtWidgets.QMessageBox.Yes:
+                    insert_query = ("INSERT INTO TBLCLI (DSCCLI, INSCLI, TELCLI, EMAIL, ENDCLI) "
+                                    "            VALUES (%s, %s, %s, %s, %s)")
+
+                    values = (nome, inscricao, telefone, email, endereco)
+
+                    # Executar a inserção
+                    self.db.execute_query(insert_query, values)
+
+                    self.db.connection.commit()
+                    # Exibir mensagem de sucesso
+                    CustomMessageBox("Sucesso", "Cliente salvo com sucesso.").information.exec_()
+                    self.accept()
+            else:
+                result = CustomMessageBox("Confirmar Gravação",
+                                          "Deseja realmente salvar esse cliente?").confirmation.exec_()
+                if result == QtWidgets.QMessageBox.Yes:
+                    # Preparar a consulta SQL para inserção de um lançamento
+                    update_query = ("UPDATE TBLCLI SET DSCCLI = %s, INSCLI = %s, TELCLI = %s, EMAIL = %s, ENDCLI = %s "
+                                    "WHERE CODCLI = %s")
+
+                    # Preparar os valores para a inserção na TBLLAN
+                    values = (nome, inscricao, telefone, email, endereco, self.codigo)
+
+                    # Executar a consulta SQL de inserção
+                    self.db.execute_query(update_query, values)
+
+                    self.db.connection.commit()
+                    # Exibir mensagem de sucesso
+                    CustomMessageBox("Sucesso", "Cliente salvo com sucesso.").information.exec_()
+                    self.accept()
+        except Exception as e:
+            self.db.connection.rollback()  # Desfaz as alterações em caso de erro
+            CustomMessageBox("Erro", f"Erro ao gravar o cliente: {str(e)}").error.exec_()
+            self.reject()
+
+    def cancelar(self):
+        self.reject()
--- UCadFornecedorDialog.py
+++ UCadFornecedorDialog.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QDialog
 from Modelos.CadFornecedor import Ui_Dialog
 from UDataBaseManager import DatabaseManager
+from UCustomMessageBox import CustomMessageBox
+from PyQt5 import QtWidgets
 
 
 class CadFornecedorDialog(QDialog, Ui_Dialog):
@@ -11,3 +13,73 @@ class CadFornecedorDialog(QDialog, Ui_Dialog):
         self.db = db
         self.state = state
         self.codigo = codigo
+
+        self.btnGravar.clicked.connect(self.gravar)
+        self.btnCancelar.clicked.connect(self.cancelar)
+        self.rdgCPF.clicked.connect(self.attMask)
+        self.rdgCNPJ.clicked.connect(self.attMask)
+
+
+    def gravar(self):
+        try:
+            if self.db.connection.in_transaction:
+                self.db.connection.rollback()
+            self.db.connection.start_transaction()
+
+            nome = self.edtNome.text()
+            if self.rdgCPF.isChecked():
+                tipoIns = "PF"
+            else:
+                tipoIns = "PJ"
+            inscricao = self.edtIns.text()
+            telefone = self.edtTelefone.text()
+            email = self.edtEmail.text()
+            endereco = self.edtEnd.text()
+
+            if self.state == "Insert":
+                result = CustomMessageBox("Confirmar Gravação",
+                                          "Deseja realmente salvar esse fornecedor?").confirmation.exec_()
+                if result == QtWidgets.QMessageBox.Yes:
+                    insert_query = ("INSERT INTO TBLFOR (DSCFOR, TIPINS, INSFOR, TELFOR, EMAIL, ENDFOR) "
+                                    "            VALUES (%s, %s, %s, %s, %s, %s")
+
+                    values = (nome, tipoIns, inscricao, telefone, email, endereco)
+
+                    # Executar a inserção
+                    self.db.execute_query(insert_query, values)
+
+                    self.db.connection.commit()
+                    # Exibir mensagem de sucesso
+                    CustomMessageBox("Sucesso", "Fornecedor gravado com sucesso.").information.exec_()
+                    self.accept()
+            else:
+                result = CustomMessageBox("Confirmar Gravação",
+                                          "Deseja realmente salvar esse fornecedor?").confirmation.exec_()
+                if result == QtWidgets.QMessageBox.Yes:
+                    # Preparar a consulta SQL para inserção de um lançamento
+                    update_query = "UPDATE TBLFOR SET DSCFOR = %s, TIPINS = %s, INSFOR = %s, TELFOR = %s,   " \
+                                   "                  EMAIL = %s, ENDFOR = %s WHERE CODFOR = %s"
+
+                    # Preparar os valores para a inserção na TBLLAN
+                    values = (nome, tipoIns, inscricao, telefone, email, endereco, self.codigo)
+
+                    # Executar a consulta SQL de inserção
+                    self.db.execute_query(update_query, values)
+
+                    self.db.connection.commit()
+                    # Exibir mensagem de sucesso
+                    CustomMessageBox("Sucesso", "Fornecedor gravado com sucesso.").information.exec_()
+                    self.accept()
+        except Exception as e:
+            self.db.connection.rollback()  # Desfaz as alterações em caso de erro
+            CustomMessageBox("Erro", f"Erro ao gravar o fornecedor: {str(e)}").error.exec_()
+            self.reject()
+
+    def cancelar(self):
+        self.reject()
+
+    def attMask(self):
+        if self.rdgCPF.isChecked():
+            self.edtIns.setInputMask('999.999.999-99')
+        else:
+            self.edtIns.setInputMask('99.999.999/9999-99')
